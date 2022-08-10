8707d0ee1635c7aaf66452169400ea419874c7b1
tanmlh <fahongzhang@gmail.com>
2022-08-10 11:35:33 +0000

    init commit

---
+++ configs/_base_/default_runtime.py
@@ -0,0 +1,38 @@
+checkpoint_config = dict(interval=1)
+# yapf:disable
+init_kwargs = {
+    'project': 'rsi-detection',
+    'entity': 'tum-tanmlh',
+}
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+        dict(type='MMDetWandbHook',
+             init_kwargs={'project': 'mmdetection'},
+             interval=10,
+             log_checkpoint=True,
+             log_checkpoint_metadata=True,
+             num_eval_images=10,
+             bbox_score_thr=0.3)
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=16)
---
+++ train.sh
@@ -0,0 +1,1 @@
+python tools/train.py configs/yolo/yolov3_d53_mstrain-608_273e_dior.py
